2a056e2b42ab748a207979a4d3c4c4ad51e70c57
N-E-W-T-O-N <new99mal@gmail.com>
2026-01-06 00:17:47 +0530

    Added Example to showcase how to use DiceDB

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,3 +4,53 @@ using DiceDB;
 
 DiceClient db = new();
 
+// map Result -> key (adapt to your proto fields)
+Func<Response, string> keySelector = r => r.Message; // change to actual key field
+
+await using var client = new DiceClient("localhost", 7379, keySelector);
+
+// Subscribe / watch a key
+var cts = new CancellationTokenSource();
+IAsyncEnumerable<Response> stream = client.WatchAsync("my-key", cts.Token);
+
+_ = Task.Run(async () =>
+{
+    await foreach (var ev in stream)
+    {
+        Console.WriteLine("Event for key: " + ev);
+    }
+});
+
+// send a command
+var cmd = new Request { Command = Commands.GET , Args = ["K"]};
+await client.SendAsync(cmd);
+
+// When done:
+cts.Cancel();         // stop subscriber
+await client.CloseAsync();
+
+
+//// map Result -> key (adapt to your proto fields)
+//Func<Result, string> keySelector = r => r.Message; // change to actual key field
+
+//await using var client = new DiceTcpClient("localhost", 7379, keySelector);
+
+//// Subscribe / watch a key
+//var cts = new CancellationTokenSource();
+//IAsyncEnumerable<Result> stream = client.WatchAsync("my-key", cts.Token);
+
+//_ = Task.Run(async () =>
+//{
+//    await foreach (var ev in stream)
+//    {
+//        Console.WriteLine("Event for key: " + ev);
+//    }
+//});
+
+//// send a command
+//var cmd = new Command { /* populate */ };
+//await client.SendAsync(cmd);
+
+//// When done:
+//cts.Cancel();         // stop subscriber
+//await client.CloseAsync();
