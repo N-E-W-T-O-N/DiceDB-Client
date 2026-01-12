cc2b09696d9cdd7a7c37708d342ffe4055991b5b
N-E-W-T-O-N <new99mal@gmail.com>
2026-01-13 00:49:27 +0530

    Added class HandleResult to better handle the Channels

---
+++ src/HandleResult.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Channels;
+using DiceDB.Generated;
+
+namespace DiceDB;
+
+public sealed class HandleResult
+{
+    private readonly Action<Result> _onResponse;
+
+    internal HandleResult(Action<Result> onResponse)
+        => _onResponse = onResponse;
+
+    public async Task RunAsync(
+        ChannelReader<byte[]> frames,
+        CancellationToken ct)
+    {
+        await foreach (var frame in frames.ReadAllAsync(ct))
+        {
+            var response = Result.Parser.ParseFrom(frame);
+            _onResponse(response);
+        }
+    }
+}
