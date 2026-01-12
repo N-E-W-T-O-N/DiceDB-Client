0b01a9a62b693ee9eeff6a17bd369a5ee2792bb6
N-E-W-T-O-N <new99mal@gmail.com>
2026-01-13 00:44:28 +0530

    Added a example Project to understand How DiceDBClient work

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,3 +4,9 @@ using DiceDB;
 
 DiceClient db = new();
 
+await db.ConnectAsync().ConfigureAwait(false);
+
+ResponseGet r = await db.GetCommandAsync("K1");
+ResponseSet re= await db.SetCommandAsync("","");
+
+await db.CloseAsync();
