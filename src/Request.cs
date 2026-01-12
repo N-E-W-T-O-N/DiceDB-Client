20bbf10eb9ddbd7710015248715364500f4726d2
N-E-W-T-O-N <new99mal@gmail.com>
2026-01-13 00:50:06 +0530

    Added Class Request which wqill be send t o user

---
+++ src/Request.cs
@@ -0,0 +1,42 @@
+﻿namespace DiceDB
+{
+    public record Response
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public ulong Fingerprint64 { get; set; }
+    }
+
+    public record ResponseGet : Response
+    {
+        public string Value { get; set; }
+    }
+
+    public record ResponseSet : Response
+    {
+    }
+
+    public record CommandList 
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string SET="SET";
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string GET= "GET"; 
+        /// <summary>
+        /// 
+        /// </summary>
+        //SET,
+        //DEL,
+        //EXISTS,
+        //INCR,
+        //DECR
+
+
+    }
+
+
+}
